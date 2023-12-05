2cc0b725128851e04c4712e00c8b7da3945abeb5
hah98 <74331454+hah98@users.noreply.github.com>
2023-12-05 20:29:42 +0100

    Index for handlers

---
+++ src/scss/JS/handlers/index.mjs
@@ -0,0 +1,10 @@
+//// Here I will import * from the other files in handlers example login
+
+
+export * from "./register.mjs";
+export * from "./login.mjs";
+export * from "./logout.mjs";
+
+/* export * from "./createPost.mjs";
+export * from "./updatePost.mjs";
+export * from "./updateProfile.mjs"; */

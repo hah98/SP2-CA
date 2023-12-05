f7d183a8a19659792caa81f0d5a3d1faae9aa749
hah98 <74331454+hah98@users.noreply.github.com>
2023-12-05 20:32:22 +0100

    Login, Register and viewAllListings handlers

---
+++ src/scss/JS/handlers/viewAllListings.mjs
@@ -0,0 +1,29 @@
+/* hsijdbjksdnskl */
+import { API_AUCTION_URL } from "../api/constants.mjs";
+
+import { authFetch } from "../api/authFetch.mjs";
+
+const action = "/listings";
+/* const method = "get"; */
+/* const method = "delete"; */
+
+export async function getListings() {
+  const getListingsUrl = `${API_AUCTION_URL}${action}`;
+
+  const response = await authFetch(getListingsUrl);
+
+  return await response.json();
+}
+
+export async function getListing(id) {
+  if (!id) {
+    throw new Error("You need postID to GET");
+  }
+  const getListingUrl = `${API_AUCTION_URL}${action}/${id}`;
+
+  const response = await authFetch(getListingUrl);
+
+  return await response.json();
+}
+
+

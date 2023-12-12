9b44c38d6f86be3e5bf5b2d01be3a5e4c50faa3d
hah98 <74331454+hah98@users.noreply.github.com>
2023-12-12 21:24:47 +0100

    small fix

--- src/scss/JS/handlers/viewAllListings.mjs
+++ src/scss/JS/handlers/viewAllListings.mjs
@@ -4,8 +4,6 @@ import { API_AUCTION_URL } from "../api/constants.mjs";
 import { authFetch } from "../api/authFetch.mjs";
 
 const action = "/listings";
-/* const method = "get"; */
-/* const method = "delete"; */
 
 export async function getListings() {
   const getListingsUrl = `${API_AUCTION_URL}${action}`;
